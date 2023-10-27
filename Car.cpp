e3e122e09e941d2efd70aed56eb4e59455f167c4
Lekantrop <98541354+Lekantrop-gd@users.noreply.github.com>
2023-10-27 12:19:53 +0300

    Add constructors and destructors

--- Car.cpp
+++ Car.cpp
@@ -1,1 +1,35 @@
 #include "Car.h"
+
+Car::Car(
+	int id, 
+	const string& model, 
+	int price, 
+	const string& registrationNumber, 
+	const string& vinCode, 
+	int numberOfSeats, 
+	int numberOfDoors)
+	: 
+	id(id), 
+	model(model), 
+	price(price), 
+	registrationNumber(registrationNumber), 
+	vinCode(vinCode), 
+	numberOfSeats(numberOfSeats), 
+	numberOfDoors(numberOfDoors)
+{
+}
+
+Car::Car(const Car& car)
+{
+	this->id = car.id;
+	this->model = car.model;
+	this->price = car.price;
+	this->registrationNumber = car.registrationNumber;
+	this->vinCode = car.vinCode;
+	this->numberOfSeats = car.numberOfSeats;
+	this->numberOfDoors = car.numberOfDoors;
+}
+
+Car::~Car()
+{
+}
--- Car.h
+++ Car.h
@@ -4,6 +4,7 @@ using namespace std;
 
 class Car
 {
+private:
 	int id;
 	string model;
 	int price;
@@ -11,4 +12,10 @@ class Car
 	string vinCode;
 	int numberOfSeats;
 	int numberOfDoors;
+
+public:
+	Car() = default;
+	Car(int id, const string& model, int price, const string& registrationNumber, const string& vinCode, int numberOfSeats, int numberOfDoors);
+	Car(const Car &car);
+	~Car();
 };
