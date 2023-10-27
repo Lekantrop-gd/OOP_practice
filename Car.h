8251806d4645615a3f0938a005e6487551b212e2
Lekantrop <98541354+Lekantrop-gd@users.noreply.github.com>
2023-10-27 12:13:20 +0300

    Initial commit

---
+++ Car.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+class Car
+{
+	int id;
+	string model;
+	int price;
+	string registrationNumber;
+	string vinCode;
+	int numberOfSeats;
+	int numberOfDoors;
+};
